aab6c977fdc742afd3d2cee3e5c029217dbd4820
fchepil <fchepil@hotmail.com>
2022-04-07 12:04:37 -0500

    Update main.py
    setup logger to track RC fax transmissions, rather than use my own function call.

--- apps/main.py
+++ apps/main.py
@@ -4,19 +4,17 @@ import logging
 from logging.handlers import RotatingFileHandler
 import datetime
 
-
 # Check initial Application environment and create if missing
 basedir = os.path.abspath(os.path.dirname(__file__))
 for xdir in ['Outbound', 'Response', 'Logs']:
     if not os.path.exists(xdir):
         os.mkdir(xdir)
 
-
 # Setup error log creation
 
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-f_handler = RotatingFileHandler(basedir+'\\Logs\\faxservice.log', maxBytes=20480, backupCount=10)
+f_handler = RotatingFileHandler(basedir + '\\Logs\\faxservice.log', maxBytes=20480, backupCount=10)
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 f_handler.setFormatter(f_format)
 logger.addHandler(f_handler)
@@ -24,15 +22,16 @@ logger.addHandler(f_handler)
 # Setup RingCentral Fax Transmission logging
 
 rc_logger = logging.getLogger()
-rc_handler = RotatingFileHandler(basedir+'\\Logs\\RC_Transmission.log', maxBytes=20480, backupCount=30)
+rc_handler = RotatingFileHandler(basedir + '\\Logs\\RC_Transmission.log', maxBytes=20480, backupCount=30)
 rc_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 rc_handler.setFormatter(f_format)
 rc_logger.addHandler(rc_handler)
 
+
 # Setup directory path for json response's from Ring Central
 
 def rc_confirm(response):
-    f_json = str(datetime.datetime.now().strftime('%Y%m%d_%H%M%S')+'_'+response)
+    f_json = str(datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '_' + response)
     f_w_response = os.path.join(basedir + '\\Response\\' + f_json)
     return f_w_response
 
